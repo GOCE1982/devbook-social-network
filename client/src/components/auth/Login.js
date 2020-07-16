c1d09ca80a1ce420f37c2daa0eb182a9dba81082
GOCE1982 <goce.ilievski@live.com>
2020-07-16 14:58:58 +0200

    Logout feature and conditional Navbar Links

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -14,6 +14,12 @@ class Login extends Component {
     };
   }
 
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
   componentDidUpdate(prevProps) {
     if (prevProps.auth.isAuthenticated !== this.props.auth.isAuthenticated) {
       this.props.history.push("/dashboard");
