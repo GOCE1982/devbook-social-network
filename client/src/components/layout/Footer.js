af6a5e31760024918ef5bc4d678155515c5b9255
GOCE1982 <goce.ilievski@live.com>
2020-07-19 23:51:49 +0200

    UI layout makeover

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-    return (
-        <footer className="bg-dark text-white mt-5 p-4 text-center">
-            Copyright &copy; {new Date().getFullYear()} DevBook
-        </footer>
-    )
+  return (
+    <footer id="footer" className="bg-dark text-white mt-5 p-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} DevBook
+    </footer>
+  );
 }
 
 export default Footer;
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -17,13 +17,16 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Developer's Social Network</h1>
+                <h1 className="display-4 mb-4">Developer's Social Network</h1>
                 <p className="lead">
-                  Create a developer profile/portfolio, share posts and get help
-                  from other developers
+                  Create a profile/portfolio, share posts and get help from
+                  other developers
                 </p>
                 <hr />
-                <Link to="/register" className="btn btn-lg btn-info mr-2">
+                <Link
+                  to="/register"
+                  className="btn btn-lg btn-outline-light mr-2"
+                >
                   Sign Up
                 </Link>
                 <Link to="/login" className="btn btn-lg btn-light">
