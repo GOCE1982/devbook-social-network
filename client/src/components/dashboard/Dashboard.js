dc230ca1139f120a3d95dcb75b725f0e21827aeb
GOCE1982 <goce.ilievski@live.com>
2020-07-21 15:08:03 +0200

    Experience Display and Delete

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import { getCurrentProfile, deleteAccount } from "../../actions/profileActions";
 import { connect } from "react-redux";
 import Spinner from "../common/Spinner";
 import ProfileActions from "./ProfileActions";
+import Exoerience from "./Experience";
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -31,7 +32,7 @@ class Dashboard extends Component {
               Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
             </p>
             <ProfileActions />
-            {/* TODO: exp and edu */}
+            <Exoerience experience={profile.experience} />
             <div style={{ marginBottom: "60px" }}>
               <button
                 onClick={this.onDeleteClick.bind(this)}
